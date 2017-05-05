e3a6e67b6958219b91a9d76c647b36c6f005bed9
Tse-Hsien Chiang <jackblackevo@gmail.com>
2017-05-05 18:41:40 +0800

    add react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {})
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Hello from './react_component'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello />,
+    document.getElementById('app')
+  )
+})
